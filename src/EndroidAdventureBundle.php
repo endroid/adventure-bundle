1370cc54e863b5aba2d42eb1860ab3c0d1496c81
Jeroen van den Enden <info@endroid.nl>
2019-01-26 23:10:25 +0000

    WIP

---
+++ src/EndroidAdventureBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Endroid\AdventureBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class EndroidAdventureBundle extends Bundle
+{
+}
